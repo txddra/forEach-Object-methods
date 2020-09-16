551fff11976793eea5f10bf55936efea8f5f11a8
LaToddra Belton <latoddra.belton@codeimmersives.com>
2020-09-16 15:03:14 -0400

    first done

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 //loop through the object using forEach so that it logs, (eg,for user1)
+
 //'user1 is called Jon and is 23 years old'
-//the loop should not return a data structure
+
+//the loop should not return a data 
+// structure
+
 // Use one or more of the object methods to solve
 const obj = {
   user1: {
@@ -16,6 +20,32 @@ const obj = {
     age: 21,
   },
 };
+/*loop though */
+
+let x= Object.entries(obj).forEach(user=>{
+  console.log(`${user[0]} is called ${user[1].name} and is ${user[1].age}`);
+});
+//this will give me the 
+
+// console.log(x)
+// // for(const key)
+// let selectUser = Object.keys(obj)
+// let userAge = Object.values(obj)
+// let together = Object.entries(obj)
+// user.forEach(element => {
+  
+// });
+
+// for(const[user, name] of Object.values(obj)){
+//   console.log(`${user} is called ${name} and is`)
+// }
+// console.log(selectUser)
+// console.log(userAge)
+// console.log(together)
+
+// console.log(`${obj.user1[1]}`)
+
+
 // Use Object Methods
 //Create an albphabeticl list of every folder in the documents object (the keys are the folders)
 //Create a list of every file in the documents object. one list.
